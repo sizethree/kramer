e13961b9cb026be3d507b0253ec0d7234c31f1e6
Danny Hadley <dadleyy@gmail.com>
2019-11-02 03:46:23 -0400

    0.2.0 - set commands (#4)
    * sadd, smembers
    
    * simplify test, ordering later
    
    * srem
    
    * sunion
    
    * scard
    
    * sdiff
    
    * sismember, sinter
    
    * ordering later
    
    * update todo file
    
    * another stopgap

---
+++ src/sets.rs
@@ -0,0 +1,217 @@
+use crate::modifiers::{format_bulk_string, Arity};
+
+#[derive(Debug)]
+pub enum SetCommand<S>
+where
+  S: std::fmt::Display,
+{
+  Add(S, Arity<S>),
+  Rem(S, Arity<S>),
+  Card(S),
+  Union(Arity<S>),
+  Inter(Arity<S>),
+  IsMember(S, S),
+  Diff(Arity<S>),
+  Members(S),
+  Pop(S, u64),
+}
+
+impl<S: std::fmt::Display> std::fmt::Display for SetCommand<S> {
+  fn fmt(&self, formatter: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+    match self {
+      SetCommand::Card(key) => write!(formatter, "*2\r\n$5\r\nSCARD\r\n{}", format_bulk_string(key)),
+      SetCommand::IsMember(key, value) => write!(
+        formatter,
+        "*3\r\n$9\r\nSISMEMBER\r\n{}{}",
+        format_bulk_string(key),
+        format_bulk_string(value)
+      ),
+
+      SetCommand::Inter(Arity::One(member)) => {
+        write!(formatter, "*2\r\n$6\r\nSINTER\r\n{}", format_bulk_string(member))
+      }
+      SetCommand::Inter(Arity::Many(members)) => {
+        let count = members.len();
+        let tail = members.iter().map(format_bulk_string).collect::<String>();
+        write!(formatter, "*{}\r\n$6\r\nSINTER\r\n{}", count + 1, tail)
+      }
+
+      SetCommand::Diff(Arity::One(member)) => write!(formatter, "*2\r\n$5\r\nSDIFF\r\n{}", format_bulk_string(member)),
+      SetCommand::Diff(Arity::Many(members)) => {
+        let count = members.len();
+        let tail = members.iter().map(format_bulk_string).collect::<String>();
+        write!(formatter, "*{}\r\n$5\r\nSDIFF\r\n{}", count + 1, tail)
+      }
+
+      SetCommand::Union(Arity::One(member)) => {
+        write!(formatter, "*2\r\n$6\r\nSUNION\r\n{}", format_bulk_string(member))
+      }
+      SetCommand::Union(Arity::Many(members)) => {
+        let count = members.len();
+        let tail = members.iter().map(format_bulk_string).collect::<String>();
+        write!(formatter, "*{}\r\n$6\r\nSUNION\r\n{}", count + 1, tail)
+      }
+
+      SetCommand::Rem(key, Arity::One(member)) => write!(
+        formatter,
+        "*3\r\n$4\r\nSREM\r\n{}{}",
+        format_bulk_string(key),
+        format_bulk_string(member)
+      ),
+      SetCommand::Rem(key, Arity::Many(members)) => {
+        let count = members.len();
+        let tail = members.iter().map(format_bulk_string).collect::<String>();
+        write!(
+          formatter,
+          "*{}\r\n$4\r\nSREM\r\n{}{}",
+          count + 2,
+          format_bulk_string(key),
+          tail
+        )
+      }
+      SetCommand::Pop(key, 1) => write!(formatter, "*2\r\n$4\r\nSPOP\r\n{}", format_bulk_string(key)),
+      SetCommand::Pop(key, amt) => write!(
+        formatter,
+        "*2\r\n$4\r\nSPOP\r\n{}{}",
+        format_bulk_string(key),
+        format_bulk_string(amt)
+      ),
+
+      SetCommand::Add(key, Arity::One(member)) => write!(
+        formatter,
+        "*3\r\n$4\r\nSADD\r\n{}{}",
+        format_bulk_string(key),
+        format_bulk_string(member)
+      ),
+      SetCommand::Add(key, Arity::Many(members)) => {
+        let count = members.len();
+        let tail = members.iter().map(format_bulk_string).collect::<String>();
+        write!(
+          formatter,
+          "*{}\r\n$4\r\nSADD\r\n{}{}",
+          count + 2,
+          format_bulk_string(key),
+          tail
+        )
+      }
+      SetCommand::Members(key) => write!(formatter, "*2\r\n$8\r\nSMEMBERS\r\n{}", format_bulk_string(key)),
+    }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::SetCommand;
+  use crate::modifiers::Arity;
+  use std::io::prelude::*;
+
+  #[test]
+  fn test_sadd_single() {
+    let cmd = SetCommand::Add("seasons", Arity::One("one"));
+    let mut buffer = Vec::new();
+    write!(buffer, "{}", cmd).expect("was able to write");
+    assert_eq!(
+      String::from_utf8(buffer).unwrap(),
+      String::from("*3\r\n$4\r\nSADD\r\n$7\r\nseasons\r\n$3\r\none\r\n")
+    );
+  }
+
+  #[test]
+  fn test_sadd_multi() {
+    let cmd = SetCommand::Add("seasons", Arity::Many(vec!["one", "two"]));
+    let mut buffer = Vec::new();
+    write!(buffer, "{}", cmd).expect("was able to write");
+    assert_eq!(
+      String::from_utf8(buffer).unwrap(),
+      String::from("*4\r\n$4\r\nSADD\r\n$7\r\nseasons\r\n$3\r\none\r\n$3\r\ntwo\r\n")
+    );
+  }
+
+  #[test]
+  fn test_smembers_multi() {
+    let cmd = SetCommand::Members("seasons");
+    let mut buffer = Vec::new();
+    write!(buffer, "{}", cmd).expect("was able to write");
+    assert_eq!(
+      String::from_utf8(buffer).unwrap(),
+      String::from("*2\r\n$8\r\nSMEMBERS\r\n$7\r\nseasons\r\n")
+    );
+  }
+
+  #[test]
+  fn test_srem_single() {
+    let cmd = SetCommand::Rem("seasons", Arity::One("one"));
+    let mut buffer = Vec::new();
+    write!(buffer, "{}", cmd).expect("was able to write");
+    assert_eq!(
+      String::from_utf8(buffer).unwrap(),
+      String::from("*3\r\n$4\r\nSREM\r\n$7\r\nseasons\r\n$3\r\none\r\n")
+    );
+  }
+
+  #[test]
+  fn test_srem_multi() {
+    let cmd = SetCommand::Rem("seasons", Arity::Many(vec!["one", "two"]));
+    let mut buffer = Vec::new();
+    write!(buffer, "{}", cmd).expect("was able to write");
+    assert_eq!(
+      String::from_utf8(buffer).unwrap(),
+      String::from("*4\r\n$4\r\nSREM\r\n$7\r\nseasons\r\n$3\r\none\r\n$3\r\ntwo\r\n")
+    );
+  }
+
+  #[test]
+  fn test_scard_multi() {
+    let cmd = SetCommand::Card("seasons");
+    let mut buffer = Vec::new();
+    write!(buffer, "{}", cmd).expect("was able to write");
+    assert_eq!(
+      String::from_utf8(buffer).unwrap(),
+      String::from("*2\r\n$5\r\nSCARD\r\n$7\r\nseasons\r\n")
+    );
+  }
+
+  #[test]
+  fn test_sdiff_single() {
+    let cmd = SetCommand::Diff(Arity::One("one"));
+    let mut buffer = Vec::new();
+    write!(buffer, "{}", cmd).expect("was able to write");
+    assert_eq!(
+      String::from_utf8(buffer).unwrap(),
+      String::from("*2\r\n$5\r\nSDIFF\r\n$3\r\none\r\n")
+    );
+  }
+
+  #[test]
+  fn test_sinter_single() {
+    let cmd = SetCommand::Inter(Arity::One("some"));
+    let mut buffer = Vec::new();
+    write!(buffer, "{}", cmd).expect("was able to write");
+    assert_eq!(
+      String::from_utf8(buffer).unwrap(),
+      String::from("*2\r\n$6\r\nSINTER\r\n$4\r\nsome\r\n")
+    );
+  }
+
+  #[test]
+  fn test_sinter_multi() {
+    let cmd = SetCommand::Inter(Arity::Many(vec!["one", "two"]));
+    let mut buffer = Vec::new();
+    write!(buffer, "{}", cmd).expect("was able to write");
+    assert_eq!(
+      String::from_utf8(buffer).unwrap(),
+      String::from("*3\r\n$6\r\nSINTER\r\n$3\r\none\r\n$3\r\ntwo\r\n")
+    );
+  }
+
+  #[test]
+  fn test_sdiff_multi() {
+    let cmd = SetCommand::Diff(Arity::Many(vec!["one", "two"]));
+    let mut buffer = Vec::new();
+    write!(buffer, "{}", cmd).expect("was able to write");
+    assert_eq!(
+      String::from_utf8(buffer).unwrap(),
+      String::from("*3\r\n$5\r\nSDIFF\r\n$3\r\none\r\n$3\r\ntwo\r\n")
+    );
+  }
+}
---
+++ tests/execute_sync_test.rs
@@ -0,0 +1,182 @@
+#![cfg(not(feature = "kramer-async"))]
+extern crate kramer;
+
+use kramer::{execute, Arity, Command, Response, ResponseValue, SetCommand};
+use std::env::var;
+
+#[cfg(test)]
+fn get_redis_url() -> String {
+  let host = var("REDIS_HOST").unwrap_or(String::from("0.0.0.0"));
+  let port = var("REDIS_PORT").unwrap_or(String::from("6379"));
+  format!("{}:{}", host, port)
+}
+
+#[test]
+fn test_sadd_single() {
+  let key = "test_sadd_single";
+  let cmd = SetCommand::Add(key, Arity::One("one"));
+  let mut con = std::net::TcpStream::connect(get_redis_url()).expect("connection");
+  let result = execute(&mut con, cmd).expect("executed");
+  execute(&mut con, Command::Del(Arity::One(key))).expect("executed");
+  assert_eq!(result, Response::Item(ResponseValue::Integer(1)));
+}
+
+#[test]
+fn test_sadd_multi() {
+  let key = "test_sadd_multi";
+  let cmd = SetCommand::Add(key, Arity::Many(vec!["one", "two"]));
+  let mut con = std::net::TcpStream::connect(get_redis_url()).expect("connection");
+  let result = execute(&mut con, cmd).expect("executed");
+  execute(&mut con, Command::Del(Arity::One(key))).expect("executed");
+  assert_eq!(result, Response::Item(ResponseValue::Integer(2)));
+}
+
+#[test]
+fn test_smembers_multi() {
+  let key = "test_smembers_multi";
+  let mut con = std::net::TcpStream::connect(get_redis_url()).expect("connection");
+  execute(&mut con, SetCommand::Add(key, Arity::Many(vec!["one"]))).expect("executed");
+  let cmd = SetCommand::Members(key);
+  let result = execute(&mut con, cmd).expect("executed");
+  execute(&mut con, Command::Del(Arity::One(key))).expect("executed");
+  assert_eq!(
+    result,
+    Response::Array(vec![ResponseValue::String(String::from("one")),])
+  );
+}
+
+#[test]
+fn test_srem_single() {
+  let key = "test_srem_single";
+  let mut con = std::net::TcpStream::connect(get_redis_url()).expect("connection");
+  execute(&mut con, SetCommand::Add(key, Arity::One("one"))).expect("executed");
+  let result = execute(&mut con, SetCommand::Rem(key, Arity::One("one"))).expect("executed");
+  execute(&mut con, Command::Del(Arity::One(key))).expect("executed");
+  assert_eq!(result, Response::Item(ResponseValue::Integer(1)));
+}
+
+#[test]
+fn test_srem_multi() {
+  let key = "test_srem_multi";
+  let mut con = std::net::TcpStream::connect(get_redis_url()).expect("connection");
+  execute(&mut con, SetCommand::Add(key, Arity::One("one"))).expect("executed");
+  execute(&mut con, SetCommand::Add(key, Arity::One("two"))).expect("executed");
+  let result = execute(&mut con, SetCommand::Rem(key, Arity::Many(vec!["one", "two"]))).expect("executed");
+  execute(&mut con, Command::Del(Arity::One(key))).expect("executed");
+  assert_eq!(result, Response::Item(ResponseValue::Integer(2)));
+}
+
+#[test]
+fn test_union_single() {
+  let key = "test_union_single";
+  let mut con = std::net::TcpStream::connect(get_redis_url()).expect("connection");
+  execute(&mut con, SetCommand::Add(key, Arity::One("one"))).expect("executed");
+  execute(&mut con, SetCommand::Add(key, Arity::One("two"))).expect("executed");
+  let result = execute(&mut con, SetCommand::Union(Arity::One(key)));
+  execute(&mut con, Command::Del(Arity::One(key))).expect("executed");
+  assert!(result.is_ok());
+}
+
+#[test]
+fn test_union_multi() {
+  let (one, two) = ("test_union_multi_1", "test_union_multi_2");
+  let mut con = std::net::TcpStream::connect(get_redis_url()).expect("connection");
+  execute(&mut con, SetCommand::Add(one, Arity::One("one"))).expect("executed");
+  execute(&mut con, SetCommand::Add(two, Arity::One("two"))).expect("executed");
+  let result = execute(&mut con, SetCommand::Union(Arity::Many(vec![one, two])));
+  execute(&mut con, Command::Del(Arity::One(one))).expect("executed");
+  execute(&mut con, Command::Del(Arity::One(two))).expect("executed");
+  assert!(result.is_ok());
+
+  // todo: ordering
+  // assert_eq!(
+  //   result,
+  //   Response::Array(vec![
+  //     ResponseValue::String(String::from("two")),
+  //     ResponseValue::String(String::from("one")),
+  //   ])
+  // );
+}
+
+#[test]
+fn test_scard() {
+  let key = "test_scard";
+  let mut con = std::net::TcpStream::connect(get_redis_url()).expect("connection");
+  execute(&mut con, SetCommand::Add(key, Arity::One("one"))).expect("executed");
+  execute(&mut con, SetCommand::Add(key, Arity::One("two"))).expect("executed");
+  let result = execute(&mut con, SetCommand::Card(key)).expect("executed");
+  execute(&mut con, Command::Del(Arity::One(key))).expect("executed");
+  assert_eq!(result, Response::Item(ResponseValue::Integer(2)));
+}
+
+#[test]
+fn test_diff_none() {
+  let (one, two) = ("test_diff_none_1", "test_diff_none_2");
+  let mut con = std::net::TcpStream::connect(get_redis_url()).expect("connection");
+  execute(&mut con, SetCommand::Add(one, Arity::One("one"))).expect("executed");
+  execute(&mut con, SetCommand::Add(two, Arity::One("two"))).expect("executed");
+  execute(&mut con, SetCommand::Add(two, Arity::One("one"))).expect("executed");
+  let result = execute(&mut con, SetCommand::Diff(Arity::Many(vec![one, two]))).expect("executed");
+  execute(&mut con, Command::Del(Arity::One(one))).expect("executed");
+  execute(&mut con, Command::Del(Arity::One(two))).expect("executed");
+  assert_eq!(result, Response::Array(vec![]));
+}
+
+#[test]
+fn test_diff_some() {
+  let one = "test_diff_some_1";
+  let mut con = std::net::TcpStream::connect(get_redis_url()).expect("connection");
+  execute(&mut con, SetCommand::Add(one, Arity::One("one"))).expect("executed");
+  let result = execute(&mut con, SetCommand::Diff(Arity::Many(vec![one]))).expect("executed");
+  execute(&mut con, Command::Del(Arity::One(one))).expect("executed");
+  assert_eq!(
+    result,
+    Response::Array(vec![ResponseValue::String(String::from("one"))])
+  );
+}
+
+#[test]
+fn test_ismember_some() {
+  let key = "test_ismember_some";
+  let mut con = std::net::TcpStream::connect(get_redis_url()).expect("connection");
+  execute(&mut con, SetCommand::Add(key, Arity::One("one"))).expect("executed");
+  let result = execute(&mut con, SetCommand::IsMember(key, "one")).expect("executed");
+  execute(&mut con, Command::Del(Arity::One(key))).expect("executed");
+  assert_eq!(result, Response::Item(ResponseValue::Integer(1)));
+}
+
+#[test]
+fn test_ismember_none() {
+  let key = "test_ismember_none";
+  let mut con = std::net::TcpStream::connect(get_redis_url()).expect("connection");
+  let result = execute(&mut con, SetCommand::IsMember(key, "one")).expect("executed");
+  execute(&mut con, Command::Del(Arity::One(key))).expect("executed");
+  assert_eq!(result, Response::Item(ResponseValue::Integer(0)));
+}
+
+#[test]
+fn test_inter_none() {
+  let (one, two) = ("test_inter_none_1", "test_inter_none_2");
+  let mut con = std::net::TcpStream::connect(get_redis_url()).expect("connection");
+  execute(&mut con, SetCommand::Add(one, Arity::One("one"))).expect("executed");
+  execute(&mut con, SetCommand::Add(two, Arity::One("two"))).expect("executed");
+  let result = execute(&mut con, SetCommand::Inter(Arity::Many(vec![one, two]))).expect("executed");
+  execute(&mut con, Command::Del(Arity::One(one))).expect("executed");
+  execute(&mut con, Command::Del(Arity::One(two))).expect("executed");
+  assert_eq!(result, Response::Array(vec![]));
+}
+
+#[test]
+fn test_inter_some() {
+  let (one, two) = ("test_inter_some_1", "test_inter_some_2");
+  let mut con = std::net::TcpStream::connect(get_redis_url()).expect("connection");
+  execute(&mut con, SetCommand::Add(one, Arity::One("one"))).expect("executed");
+  execute(&mut con, SetCommand::Add(two, Arity::One("one"))).expect("executed");
+  let result = execute(&mut con, SetCommand::Inter(Arity::Many(vec![one, two]))).expect("executed");
+  execute(&mut con, Command::Del(Arity::One(one))).expect("executed");
+  execute(&mut con, Command::Del(Arity::One(two))).expect("executed");
+  assert_eq!(
+    result,
+    Response::Array(vec![ResponseValue::String(String::from("one"))])
+  );
+}
